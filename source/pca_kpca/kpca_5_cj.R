9a1ebc0532b2eac1ab275b3678c8af935fd8b46d
CJ Yetman <cj@cjyetman.com>
2018-05-07 20:16:15 +0200

    process randomly sampled cuts of the corpus

--- source/pca_kpca/kpca_5_cj.R
+++ source/pca_kpca/kpca_5_cj.R
@@ -7,7 +7,8 @@
 # !! Must set working directory to the location of the EIUCrisesMeasure repository
 
 # which file to use
-rds_file <- "eiu_texts_2016_2018.rds"
+rda_file <- "source/pca_kpca/preprocessed_data/eiu_texts_from_2003.rda"
+sample_cuts <- 10
 
 # Load packages
 library(simpleSetup)
@@ -22,27 +23,44 @@ possible_dir <- c('/Users/cjyetman/Documents/github/FinStress-gr18/',
 simpleSetup::set_valid_wd(possible_dir)
 
 # Load preprocessed data (see source/preprocess_eiu.R)
-eiu_list <- readRDS(file.path('source/pca_kpca/preprocessed_data', rds_file))
+load(rda_file)
+# eiu_list <- readRDS(file.path('source/pca_kpca/preprocessed_data', rds_file))
 
-# Extract identifying country names and document dates
-country_date <- names(eiu_list)
-country_date <- stringr::str_split(country_date, pattern = '_', n = 2,
-                                   simplify = TRUE) %>% as.data.frame
-names(country_date) <- c('iso3c', 'date')
-country_date$country <- countrycode(country_date$iso3c, origin = 'iso3c',
-                                    destination = 'country.name')
-country_date <- country_date[, c('country', 'iso3c', 'date')]
+set.seed(1234)
+rand_seq <- sample(seq_along(eiu_list))
+sample_sets <- split(rand_seq, seq_along(rand_seq) %% sample_cuts)
+
+lapply(sample_sets, range)
+lapply(sample_sets, length)
 
 # Source the function for conducting KPCA/refining/saving the results
 source('source/pca_kpca/setup/kpca_eiu_function.R')
 
-# Run KPCA
-system.time(
-  kpca_eiu(eiu_list, country_date, length_spec = 5, n_period = 2,
-           out_dir = 'source/pca_kpca/raw_data_output/5_strings/')
-)
+all_out_dir <- 'source/pca_kpca/output'
+if (!dir.exists(all_out_dir)) { dir.create(all_out_dir) }
 
-# notify me with a push message
-pushover(message = paste(rds_file, "is done"),
-         user = "uffbujtdi51jm3kk1vcgcjog699a85",
-         app = "adk4twf314vctkfdbe95vb35f4mrsz")
+for (i in seq_along(sample_sets)) {
+
+  cat(paste0('\n\nrunning sample: ', i, ' of ', length(sample_sets), "\n"))
+  corpus <- eiu_list[sample_sets[[i]]]
+
+  # Extract identifying country names and document dates
+  country_date <- names(corpus)
+  country_date <- stringr::str_split(country_date, pattern = '_', n = 2,
+                                     simplify = TRUE) %>% as.data.frame
+  names(country_date) <- c('iso3c', 'date')
+  country_date$country <- countrycode(country_date$iso3c, origin = 'iso3c',
+                                      destination = 'country.name')
+  country_date <- country_date[, c('country', 'iso3c', 'date')]
+
+  out_dir <- file.path('source/pca_kpca/output', paste0('sample', i))
+  if (!dir.exists(out_dir)) { dir.create(out_dir) }
+  save(corpus, country_date, sample_sets, i, file = file.path(out_dir, 'env_sav.Rds'))
+
+  # Run KPCA
+  gc()
+  print(system.time(
+    kpca_eiu(corpus, country_date, length_spec = 5, n_period = 2,
+             out_dir = out_dir)
+  ))
+}
